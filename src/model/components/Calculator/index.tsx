b0f3f5278bffec4cf5311632261c099e11f863c2
Vergil <zaq9716643@gmail.com>
2020-06-11 16:35:48 +0800

    use better function

--- src/model/components/Calculator/index.tsx
+++ src/model/components/Calculator/index.tsx
@@ -9,38 +9,62 @@ import { setCalcVal } from '../../appRedux/actions';
 import { MathSymbolEnum } from '../../enums';
 import RaiusBarButton from '../RadiusBarButton';
 type Props = {
-    calcVal: string;
-    setCalcVal: typeof setCalcVal;
+    // calcVal: string;
+    // setCalcVal: typeof setCalcVal;
 }
 
-const signGroup1 = [MathSymbolEnum.plus, MathSymbolEnum.minus, MathSymbolEnum.divide, MathSymbolEnum.multiply, MathSymbolEnum.reverse, MathSymbolEnum.percent]
+const addOrMinus = [MathSymbolEnum.plus, MathSymbolEnum.minus]
+const divideOrMultiply = [MathSymbolEnum.divide, MathSymbolEnum.multiply]
+const reverseOrPercent = [MathSymbolEnum.reverse, MathSymbolEnum.percent]
 
 const Calculator: FC<Props> = (props) => {
 
     const [curVal, setCurVal] = useState<string>("0");
-    const [isSignedSymbol, setIsSignedSymbol] = useState(false);   //　若true則下次的number按鈕按下時複寫curVal
-    const [symbol, setSymbol] = useState<MathSymbolEnum>(MathSymbolEnum.none) // 按下按鈕的類型
+    const [calculation, setCalculation] = useState<string[]>([]);
+    const [symbol, setSymbol] = useState<MathSymbolEnum>(MathSymbolEnum.none);
+    const [flag, setFlag] = useState(false);
+    const [showType, setShowType] = useState<"calc" | "cur">("cur")
     const grayBtnTheme = { bgColor: "#AFAFAF", textColor: "#333" }
     const blueBtnTheme = { bgColor: "#3091FD", textColor: "#fff" }
     const darkBtnTheme = { bgColor: "#333", textColor: "#fff" }
 
     //數字點擊
-    const handleNumClick = (numStr: string) => {
+    const handleNumClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, numStr: string) => {
 
-        let _val = isSignedSymbol ? "0" : curVal
-        const isIncluesDot = _val.includes(".");
-
-        if (_val === "0" && numStr === "0") return
-        if (isIncluesDot && numStr === ".") return
-
-        // 容許長度為９個數字，包含小數點為１０位
-        if (isIncluesDot ? _val.length < 10 : _val.length < 9) {
-            _val += numStr
-            _val = $$.filterZeroHead(_val)
+        if (symbol === MathSymbolEnum.equal) {
+            setCalculation([])
         }
 
-        setCurVal(_val)
-        setIsSignedSymbol(false);
+        let val = curVal;
+
+        const isIncluesDot = val.includes(".");
+        if (isIncluesDot && numStr === ".") return
+
+        if (flag) {
+            val = "0"
+            // 按下數字的時候將運算符號儲存
+            if (symbol === MathSymbolEnum.plus)
+                setCalculation([...calculation, "+"])
+            if (symbol === MathSymbolEnum.minus)
+                setCalculation([...calculation, "-"])
+            if (symbol === MathSymbolEnum.multiply)
+                setCalculation([...calculation, "*"])
+            if (symbol === MathSymbolEnum.divide)
+                setCalculation([...calculation, "/"])
+        }
+
+
+
+        // 容許長度為９個數字，包含小數點為１０位
+        if (isIncluesDot ? val.length < 10 : val.length < 9) {
+            val += numStr
+            val = $$.filterZeroHead(val)
+        }
+
+        setCurVal(val)
+        setShowType("cur")
+        setSymbol(MathSymbolEnum.number);
+        setFlag(false);
     }
 
     // AC
@@ -48,114 +72,105 @@ const Calculator: FC<Props> = (props) => {
         e.preventDefault()
 
         setCurVal("0")
-        props.setCalcVal("0")
-
-        takeSign(MathSymbolEnum.clean)
+        setCalculation([])
+        setSymbol(MathSymbolEnum.clean)
+        setFlag(false);
     }
 
-    // 正負反轉
-    const handleReverseSign = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault()
+    const handleReverse = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
 
-        if (isSignedSymbol) {
-            props.setCalcVal($$.reverseSign(props.calcVal))
-        } else {
-            setCurVal($$.reverseSign(curVal))
-        }
-
-        takeSign(MathSymbolEnum.reverse)
-        // setSymbol(MathSymbolEnum.reverse)
+        // if ()
+        setSymbol(MathSymbolEnum.reverse)
+        setFlag(false);
     }
 
     // 加減乘除
     const handleCalculate = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, ms: MathSymbolEnum) => {
         e.preventDefault()
 
-        if (symbol !== MathSymbolEnum.equal) {
-            if (signGroup1.includes(symbol)) {
-                //連續加減乘除的情況，會一直求值
-                doCalculate()
-            }
-            else {
-                props.setCalcVal(curVal);
-            }
+        if (addOrMinus.includes(ms)) {
+            setShowType("calc")
+        }
+        else if (divideOrMultiply.includes(ms)) {
+            setShowType("cur")
         }
 
-        takeSign(ms)
+        if (symbol === MathSymbolEnum.number) {
+            setCalculation([...calculation, curVal])
+        }
+
+        setFlag(true);
+        setSymbol(ms);
     }
 
-    //　計算
-    const doCalculate = (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e?.preventDefault()
+    const hanldeEqual = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
 
-        let result = curVal;
-        if (symbol === MathSymbolEnum.plus) {
-            result = $$.add(+props.calcVal, +curVal).toString()
-        }
-        else if (symbol === MathSymbolEnum.minus) {
-            result = $$.minus(+props.calcVal, +curVal).toString()
-        }
-        else if (symbol === MathSymbolEnum.divide) {
-            result = $$.divide(+props.calcVal, +curVal).toString()
-        }
-        else if (symbol === MathSymbolEnum.multiply) {
-            result = $$.multiply(+props.calcVal, +curVal).toString()
-        }
-        else {
-            result = props.calcVal;
-        }
 
-        if (result === "Infinity") return
 
-        props.setCalcVal(result)
-
-        takeSign(MathSymbolEnum.equal)
+        const result = eval(calculation.concat(curVal).join(""));
+        if (result) {
+            setCalculation([result]);
+            setCurVal("");
+            setShowType("calc");
+            setFlag(false);
+            setSymbol(MathSymbolEnum.equal)
+        }
     }
 
-    const takeSign = (s: MathSymbolEnum, ) => {
-        setSymbol(s)
-        setIsSignedSymbol(true);
-    }
+    const answer = useMemo(() => {
+        if (showType === "calc") {
 
-    const showNumStr = useMemo(() => {
-        return isSignedSymbol ? props.calcVal : curVal
-    }, [isSignedSymbol, props.calcVal, curVal])
+            return eval(calculation.join("")) || "0"
+        } else {
+            return curVal
+        }
+    }, [showType, curVal, calculation])
 
     return (
         <div className="calculator-container">
             <section className="result-section">
-                <span className="result-content" style={{ fontSize: showNumStr.length > 8 ? `calc(3.8rem - ${showNumStr.length * 1.2}px)` : "3.8rem" }}>
-                    {showNumStr}
+
+                <span className="result-content" style={{ fontSize: answer.length > 8 ? `calc(3.8rem - ${answer.length * 1.2}px)` : "3.8rem" }}>
+                    {answer}
+                </span>
+
+                <span className="result-content" style={{ fontSize: curVal.length > 8 ? `calc(3.8rem - ${curVal.length * 1.2}px)` : "3.8rem" }}>
+                    {curVal}
+                </span>
+                <span className="result-content" style={{ fontSize: "1rem" }}>
+                    {`[${calculation.toString()}]`}
                 </span>
             </section>
             <div className="calculator-btn-row">
                 <CircleButton text="AC" backgroundColor={grayBtnTheme.bgColor} textColor={grayBtnTheme.textColor} onClick={cleanResult} />
-                <CircleButton text="+/-" backgroundColor={grayBtnTheme.bgColor} textColor={grayBtnTheme.textColor} onClick={handleReverseSign} />
+                <CircleButton text="+/-" backgroundColor={grayBtnTheme.bgColor} textColor={grayBtnTheme.textColor} onClick={(e) => handleCalculate(e, MathSymbolEnum.reverse)} />
                 <CircleButton text="%" backgroundColor={grayBtnTheme.bgColor} textColor={grayBtnTheme.textColor} onClick={() => { }} />
                 <CircleButton text="÷" backgroundColor={blueBtnTheme.bgColor} textColor={blueBtnTheme.textColor} onClick={(e) => handleCalculate(e, MathSymbolEnum.divide)} />
             </div>
             <div className="calculator-btn-row">
-                <CircleButton text="7" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={() => handleNumClick("7")} />
-                <CircleButton text="8" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={() => handleNumClick("8")} />
-                <CircleButton text="9" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={() => handleNumClick("9")} />
+                <CircleButton text="7" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={(e) => handleNumClick(e, "7")} />
+                <CircleButton text="8" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={(e) => handleNumClick(e, "8")} />
+                <CircleButton text="9" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={(e) => handleNumClick(e, "9")} />
                 <CircleButton text="X" backgroundColor={blueBtnTheme.bgColor} textColor={blueBtnTheme.textColor} onClick={(e) => handleCalculate(e, MathSymbolEnum.multiply)} />
             </div>
             <div className="calculator-btn-row">
-                <CircleButton text="4" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={() => handleNumClick("4")} />
-                <CircleButton text="5" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={() => handleNumClick("5")} />
-                <CircleButton text="6" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={() => handleNumClick("6")} />
+                <CircleButton text="4" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={(e) => handleNumClick(e, "4")} />
+                <CircleButton text="5" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={(e) => handleNumClick(e, "5")} />
+                <CircleButton text="6" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={(e) => handleNumClick(e, "6")} />
                 <CircleButton text="-" backgroundColor={blueBtnTheme.bgColor} textColor={blueBtnTheme.textColor} onClick={(e) => handleCalculate(e, MathSymbolEnum.minus)} />
             </div>
             <div className="calculator-btn-row">
-                <CircleButton text="1" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={() => handleNumClick("1")} />
-                <CircleButton text="2" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={() => handleNumClick("2")} />
-                <CircleButton text="3" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={() => handleNumClick("3")} />
+                <CircleButton text="1" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={(e) => handleNumClick(e, "1")} />
+                <CircleButton text="2" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={(e) => handleNumClick(e, "2")} />
+                <CircleButton text="3" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={(e) => handleNumClick(e, "3")} />
                 <CircleButton text="+" backgroundColor={blueBtnTheme.bgColor} textColor={blueBtnTheme.textColor} onClick={(e) => handleCalculate(e, MathSymbolEnum.plus)} />
             </div>
             <div className="calculator-btn-row">
-                <RaiusBarButton text="0" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={() => handleNumClick("0")} />
-                <CircleButton text="." backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={() => handleNumClick(".")} />
-                <CircleButton text="=" backgroundColor={blueBtnTheme.bgColor} textColor={blueBtnTheme.textColor} onClick={doCalculate} />
+                <RaiusBarButton text="0" backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={(e) => handleNumClick(e, "0")} />
+                <CircleButton text="." backgroundColor={darkBtnTheme.bgColor} textColor={darkBtnTheme.textColor} onClick={(e) => handleNumClick(e, ".")} />
+                <CircleButton text="=" backgroundColor={blueBtnTheme.bgColor} textColor={blueBtnTheme.textColor} onClick={hanldeEqual} />
             </div>
         </div>
     );
--- src/model/enums/index.ts
+++ src/model/enums/index.ts
@@ -1,5 +1,6 @@
 export enum MathSymbolEnum {
     none,
+    number,
     plus,
     minus,
     multiply,
